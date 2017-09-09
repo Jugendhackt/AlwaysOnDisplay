4500c3c64ef3915cec1da918fd076bed92f16738
kadse <kadse@kadse.org>
2017-09-09 20:50:10 +0200

    datenbankstuff, datenbank neu hinzugefügt.

--- pythonteststuff/http_request.py
+++ pythonteststuff/http_request.py
@@ -1,6 +1,18 @@
 from flask import Flask,request
 from flask import jsonify
+import sqlite3
 app = Flask(__name__)
+
+
+sqlite_file = 'always_on_db.sqlite'
+
+
+table_name = 'notifications'
+ #title=request.form["title"],
+        #text=request.form["text"],
+        #time=request.form["time"]
+
+
 
 @app.route('/summary')
 def summary():
@@ -18,13 +30,35 @@ def summary():
 def receiver():
     #print(request.args["param"])
     #print(request.form["param1"])
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+    title = request.form["title"]
+    text = request.form["text"]
+    time = request.form["time"]
+    id_column = "title"
+    column_name1 = "text"
+    column_name2 = "time"
+    
+    try:
+        sql = "INSERT INTO {tn} ({idf}, {cn1}, {cn2}) VALUES ('{title}', '{text}', '{time}')".\
+        format(tn=table_name, idf=id_column, title=title, text=text, time=time, cn1=column_name1, cn2=column_name2)
+        print(sql)
+        c.execute(sql)
+    except sqlite3.IntegrityError:
+        print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+
+
+
+    conn.commit()
+    #conn.close()
+
     return jsonify (
         success=True,
         failure=False,
         #channel=channel,
         #anfrage=request.get_json(force=true)
         #param2=request.args["param2"],
-        title=request.form["param1"],
-        text=request.form["param2"],
-        time=request.form["param3"]
+        title=request.form["title"],
+        text=request.form["text"],
+        time=request.form["time"]
     )
