b53760e148c4083e3fa9c923fa17f2834d54993d
kadse <kadse@kadse.org>
2017-09-09 23:35:29 +0200

    sql gefixt und sender methode fertigestellt

--- pythonteststuff/http_request.py
+++ pythonteststuff/http_request.py
@@ -32,45 +32,53 @@ def receiver():
     #print(request.form["param1"])
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
-    title = request.form["title"]
-    text = request.form["text"]
-    time = request.form["time"]
-    id_column = "title"
-    column_name1 = "text"
-    column_name2 = "time"
+    time_value = request.form["time"]
+    text_value = request.form["text"]
+    title_value = request.form["title"]
+    
+    
+    id_column_primary = "time"
+    column_text = "text"
+    column_title = "title"
     
     try:
-        sql = 'INSERT INTO {tn} ({idf}, {cn1}, {cn2}) VALUES ("{title}", "{text}", "{time}")'.\
-        format(tn=table_name, idf=id_column, title=title, text=text, time=time, cn1=column_name1, cn2=column_name2)
+        sql = 'INSERT INTO {tn} ({idf}, {cn1}, {cn2}) VALUES ("{timev}", "{txtv}", "{tv}")'.\
+        format(tn=table_name, idf=id_column_primary, cn1=column_text, cn2=column_title, txtv=text_value, timev=time_value, tv=title_value)
         print(sql)
         c.execute(sql)
     except sqlite3.IntegrityError:
-        print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
-
-
+        print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column_primary))
 
     conn.commit()
+    
+    return jsonify (
+        title=title_value,
+        text=text_value,
+        time=time_value
+    )
+
+
+
+    
     #conn.close()
 @app.route('/sender', methods=["GET"])
 def sender():
     conn = sqlite3.connect(sqlite_file)
-    c = conn.cursor()
-    title = request.form["title"]
-    text = request.form["text"]
-    time = request.form["time"]
-    
-    
+    c = conn.cursor()   
+    id_column_primary = "time"    
 
     sql = "SELECT * FROM {tn} ORDER BY {time} DESC LIMIT 10" .\
-    format(tn=table_name, time=time)
-    print(sql)
+    format(tn=table_name, time=id_column_primary)
     c.execute(sql)
-    ''' return jsonify (
-
+    all_rows = c.fetchall()
+    print('1):', all_rows)
+    return jsonify (
+        output=all_rows,
         #channel=channel,
         #anfrage=request.get_json(force=true)
         #param2=request.args["param2"],
-        title=request.form["title"],
-        text=request.form["text"],
-        time=request.form["time"]
-    )'''
+        #title=request.form["title"],
+        #text=request.form["text"],
+        #time=request.form["time"]
+        ok=True
+    )
